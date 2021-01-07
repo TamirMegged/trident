6ca660b64b047aa9a0c7a50ce98600811f7764bd
TamirMegged <tamir.m.nx@gmail.com>
2021-01-07 19:07:21 +0200

    pictures url fix

--- src/app/components/home/about/about.component.ts
+++ src/app/components/home/about/about.component.ts
@@ -10,12 +10,12 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   public slides = [
-    { image: '../../../assets/slides/kitesurfing.jpg', text: "Kitesurfing" },
-    { image: '../../../assets/slides/surfing.jpg', text: "Surfing" },
-    { image: '../../../assets/slides/scubaDiving.jpg', text: "Scuba Diving" },
-    { image: '../../../assets/slides/freediving.jpg', text: "Freediving" },
-    { image: '../../../assets/slides/spearfishing.jpg', text: "Spearfishing" },
-    { image: '../../../assets/slides/fishing.jpg', text: "Fishing" },
+    { image: 'assets/slides/kitesurfing.jpg', text: "Kitesurfing" },
+    { image: 'assets/slides/surfing.jpg', text: "Surfing" },
+    { image: 'assets/slides/scubaDiving.jpg', text: "Scuba Diving" },
+    { image: 'assets/slides/freediving.jpg', text: "Freediving" },
+    { image: 'assets/slides/spearfishing.jpg', text: "Spearfishing" },
+    { image: 'assets/slides/fishing.jpg', text: "Fishing" },
   ];
 
   ngOnInit(): void {

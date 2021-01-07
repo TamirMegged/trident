426a89cb8903da64b8a62571083e0358e42eaaff
TamirMegged <tamir.m.nx@gmail.com>
2021-01-07 20:08:43 +0200

    server

--- src/app/components/home/about/about.component.ts
+++ src/app/components/home/about/about.component.ts
@@ -10,12 +10,12 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   public slides = [
-    { image: 'https://tridentmarinesports.herokuapp.com/public//slides/kitesurfing.jpg', text: "Kitesurfing" },
-    { image: 'https://tridentmarinesports.herokuapp.com/public//slides/surfing.jpg', text: "Surfing" },
-    { image: 'https://tridentmarinesports.herokuapp.com/public//slides/scubaDiving.jpg', text: "Scuba Diving" },
-    { image: 'https://tridentmarinesports.herokuapp.com/public//slides/freediving.jpg', text: "Freediving" },
-    { image: 'https://tridentmarinesports.herokuapp.com/public//slides/spearfishing.jpg', text: "Spearfishing" },
-    { image: 'https://tridentmarinesports.herokuapp.com/public//slides/fishing.jpg', text: "Fishing" },
+    { image: 'https://tridentmarinesports.herokuapp.com/public/slides/kitesurfing.jpg', text: "Kitesurfing" },
+    { image: 'https://tridentmarinesports.herokuapp.com/public/slides/surfing.jpg', text: "Surfing" },
+    { image: 'https://tridentmarinesports.herokuapp.com/public/slides/scubaDiving.jpg', text: "Scuba Diving" },
+    { image: 'https://tridentmarinesports.herokuapp.com/public/slides/freediving.jpg', text: "Freediving" },
+    { image: 'https://tridentmarinesports.herokuapp.com/public/slides/spearfishing.jpg', text: "Spearfishing" },
+    { image: 'https://tridentmarinesports.herokuapp.com/public/slides/fishing.jpg', text: "Fishing" },
   ];
 
   ngOnInit(): void {
